4b14ec084d39d8a61306488455004413f9b8a8c0
cimarinho <marinho.marcelo@gmail.com>
2023-10-22 09:59:20 -0300

    Init project

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	val kotlinVersion = "1.8.22"
+	id("idea")
+	id("org.springframework.boot") version "3.1.4"
+	id("io.spring.dependency-management") version "1.1.3"
+	kotlin("jvm") version kotlinVersion
+	kotlin("plugin.spring") version kotlinVersion
+}
+
+group = "br.com.requestReply"
+version = "1.0.0"
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+extra["springCloudVersion"] = "2022.0.1"
+
+sourceSets {
+	main {
+		kotlin {
+			srcDir("src/main/kotlin")
+		}
+	}
+}
+
+dependencies {
+	implementation("org.javamoney:moneta:1.4.2")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.12.0")
+
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.bootJar {
+	mainClass = "br.com.requestReply"
+}
+
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
+
