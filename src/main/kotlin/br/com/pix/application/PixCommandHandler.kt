5e5bd94638934c1105fcef75b6977806d1802bd0
Marcelo Marinho <marcelo.marinho@n26brasil.com>
2023-10-30 17:31:18 -0300

    Adjust package

--- src/main/kotlin/br/com/pix/application/PixCommandHandler.kt
+++ src/main/kotlin/br/com/pix/application/PixCommandHandler.kt
@@ -3,6 +3,7 @@ package br.com.pix.application
 import br.com.pix.configuration.RedisMessageSubscriber
 import br.com.pix.infrastructure.pix.sendEvent.PixSendEvent
 import org.springframework.stereotype.Service
+import java.util.concurrent.TimeUnit
 
 @Service
 class PixCommandHandler(
@@ -13,7 +14,8 @@ class PixCommandHandler(
         pixSendEvent.sendMessage(command.toEvent())
 
         val future = messageSubscriber.subscribeToChannel(command.correlationId)
-        val messageRedis = future.join()
+
+        val messageRedis = future.get(3,  TimeUnit.SECONDS)
 
         println("Received message: $messageRedis on channel: ${command.correlationId}")
     }
